3e723e0389d994c78aaef4d2a8eda766d93f6089
crazybenny <crazybenny@29a1b359-670f-4fa0-9745-e3f699048d19>
2008-10-22 19:10:35 +0000

    - converted to Ace3 - TODO: localisation support

--- trunk/SellJunk.lua
+++ trunk/SellJunk.lua
@@ -1,98 +1,61 @@
-local options = {
-	type = 'group',
-	args = {
-		list = {
-			type = 'execute',
-			name = 'list',
-			desc = 'Lists all exceptions',
-			func = 'List'
+SellJunk = LibStub("AceAddon-3.0"):NewAddon("SellJunk", "AceConsole-3.0","AceEvent-3.0")
+local addon	= LibStub("AceAddon-3.0"):GetAddon("SellJunk")
+local AceConfigRegistry = LibStub("AceConfigRegistry-3.0")
+local AceConfigDialog 	= LibStub("AceConfigDialog-3.0")
+
+addon.optionsFrame = {}
+local options = nil
+
+addon.sellButton = CreateFrame("Button", nil, MerchantFrame, "OptionsButtonTemplate")
+addon.sellButton:SetPoint("TOPRIGHT", -41, -40)
+addon.sellButton:SetText("Sell Junk")
+addon.sellButton:SetScript("OnClick", function() SellJunk:Sell() end)
+
+function addon:OnInitialize()
+	self:RegisterChatCommand("selljunk", "OpenOptions")
+	self:RegisterChatCommand("sj", "OpenOptions")
+
+	self.db = LibStub("AceDB-3.0"):New("SellJunkDB")
+	self.db:RegisterDefaults({
+		char = {
+			exceptions = {},
+			auto = false,
 		},
-		auto = {
-			type = 'toggle',
-			name = 'auto',
-			desc = 'Toggles the automatic selling of junk when the merchant window is opened.',
-			get = 'IsAuto',
-			set = 'ToggleAuto'
-		},
-		add = {
-			type = 'text',
-			name = 'add',
-			desc = 'Add an exception for all characters.',
-			usage = '<Item Link>',
-			get = false,
-			set = 'Add'
-		},
-		rem = {
-			type = 'text',
-			name = 'rem',
-			desc = 'Remove an exception for all characters.',
-			usage = '<Item Link>',
-			get = false,
-			set = 'Rem'
-		},
-		me = {
-			type = 'group',
-			name = 'me',
-			desc = 'Contains the ability to add/remove exceptions for this character.',
-			args = {
-				add = {
-					type = 'text',
-					name = 'add',
-					desc = 'Add an exception for this characters.',
-					usage = '<Item Link>',
-					get = false,
-					set = 'AddMe'
-				},
-				rem = {
-					type = 'text',
-					name = 'rem',
-					desc = 'Remove an exception for this characters.',
-					usage = '<Item Link>',
-					get = false,
-					set = 'RemMe'
-				},
+		global = {
+			exceptions = {},
 		}
-	}
-}
-}
+	})
 
-SellJunk = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0","AceDB-2.0","AceEvent-2.0")
-local SellJunk = SellJunk
-SellJunk:RegisterChatCommand({"/SellJunk","/sj"},options)
-SellJunk:RegisterDB("SellJunkDB","SellJunkDBPC")
-SellJunk:RegisterDefaults("char", {exception = {}, auto = false })
-SellJunk:RegisterDefaults("account", {exception = {} })
+	self:PopulateOptions()
+	AceConfigRegistry:RegisterOptionsTable("SellJunk", options)
+	addon.optionsFrame = AceConfigDialog:AddToBlizOptions("SellJunk", nil, nil, "general")
+end
 
-local sellButton = CreateFrame("Button", nil, MerchantFrame, "OptionsButtonTemplate")
-sellButton:SetPoint("TOPRIGHT", -41, -40)
-sellButton:SetText("Sell Junk")
-sellButton:SetScript("OnClick", function() SellJunk:Sell() end)
-
-function SellJunk:OnEnable()
+function addon:OnEnable()
 	self:RegisterEvent("MERCHANT_SHOW")
 end
 
-function SellJunk:MERCHANT_SHOW()
-	if self.db.char.auto then
-		SellJunk:Sell()
+function addon:MERCHANT_SHOW()
+	if addon.db.char.auto then
+		self:Sell()
 	end
 end
 
-function SellJunk:IsAuto()
-	return self.db.char.auto
+function addon:IsAuto()
+	return addon.db.char.auto
 end
 
-function SellJunk:ToggleAuto()
+function addon:ToggleAuto()
 	self.db.char.auto = not self.db.char.auto
 end
 
-function SellJunk:Sell()
+function addon:Sell()
 	for bag = 0,4 do
 		for slot = 1,GetContainerNumSlots(bag) do
 			local item = GetContainerItemLink(bag,slot)
 			if item then
 				local found = string.find(item,"|cff9d9d9d")
-				if ((found) and (not SellJunk:isException(item))) or ((not found) and (SellJunk:isException(item))) then
+				if ((found) and (not addon:isException(item))) or ((not found) and (addon:isException(item))) then
 					PickupContainerItem(bag,slot)
 					PickupMerchantItem()
 					self:Print("Sold " .. item)
@@ -102,108 +65,89 @@ function SellJunk:Sell()
 	end
 end
 
-function SellJunk:List()
+function addon:List()
+	self:Print("listing:")
 	local test
-	if self.db.account.exception then
+	if self.db.global.exceptions then
 		self:Print("Global exception list:")
-		for k,v in pairs(self.db.account.exception) do
+		for k,v in pairs(self.db.global.exceptions) do
 			self:Print(v)
 		end
 	end
 
-	if self.db.char.exception then
+	if self.db.char.exceptions then
 		self:Print("Character exception list:")
-		for k,v in pairs(self.db.char.exception) do
+		for k,v in pairs(self.db.char.exceptions) do
 			self:Print(v)
 		end
 	end
 end
 
-function SellJunk:Add(link)
-	self.db.account.exception[table.getn(self.db.account.exception) + 1] = link
-	self:Print(link .. " added to global exception list.")	
+function addon:Add(link, global)
+	if global then
+		self.db.global.exceptions[table.getn(self.db.global.exceptions) + 1] = link
+		self:Print(link .. " added to global exception list.")
+	else
+		self.db.char.exceptions[table.getn(self.db.char.exceptions) + 1] = link
+		self:Print(link .. " added to character exception list.")
+	end		
 end
 
-function SellJunk:Rem(link)
+function addon:Rem(link, global)
 	local found = false
 	local exception
 	local _, _, linkID = string.find(link,"item:(%d+)")
-	for k,v in pairs(self.db.account.exception) do
-		_, _, exception = string.find(v,"item:(%d+)")
-		if (exception == linkID) then
-			found = true
-		end
-		if found then
-			if self.db.account.exception[k+1] then
-				self.db.account.exception[k] = self.db.account.exception[k+1]
-			else
-				self.db.account.exception[k] = nil
+	
+	if global then
+		for k,v in pairs(self.db.global.exceptions) do
+			_, _, exception = string.find(v,"item:(%d+)")
+			if (exception == linkID) then
+				found = true
+			end
+			if found then
+				if self.db.global.exceptions[k+1] then
+					self.db.global.exceptions[k] = self.db.global.exceptions[k+1]
+				else
+					self.db.global.exceptions[k] = nil
+				end
 			end
 		end
-	end
-	if found then
-		self:Print("Removed " .. link .. " from global exception list.")
-	end
-	found = false
-
-	for k,v in pairs(self.db.char.exception) do
-		_, _, exception = string.find(v,"item:(%d+)")
-		if (exception == linkID) then
-			found = true
-		end
 		if found then
-			if self.db.char.exception[k+1] then
-				self.db.char.exception[k] = self.db.char.exception[k+1]
-			else
-				self.db.char.exception[k] = nil
+			self:Print("Removed " .. link .. " from global exception list.")
+		end
+	else
+		for k,v in pairs(self.db.char.exceptions) do
+			_, _, exception = string.find(v,"item:(%d+)")
+			if (exception == linkID) then
+				found = true
+			end
+			if found then
+				if self.db.char.exceptions[k+1] then
+					self.db.char.exceptions[k] = self.db.char.exceptions[k+1]
+				else
+					self.db.char.exceptions[k] = nil
+				end
 			end
 		end
-	end
-	if found then
-		self:Print("Removed " .. link .. " from exception exception list.")
+		if found then
+			self:Print("Removed " .. link .. " from character's exception list.")
+		end
 	end
 end
 
-function SellJunk:AddMe(link)
-	self.db.char.exception[table.getn(self.db.char.exception) + 1] = link
-	self:Print(link .. " added to character exception list.")
-end
-
-function SellJunk:RemMe(link)
-	local exception
-	local _, _, linkID = string.find(link,"item:(%d+)")
-	local found = false
-	for k,v in pairs(self.db.char.exception) do
-		_, _, exception = string.find(v,"item:(%d+)")
-		if (exception == linkID) then
-			found = true
-		end
-		if found then
-			if self.db.char.exception[k+1] then
-				self.db.char.exception[k] = self.db.char.exception[k+1]
-			else
-				self.db.char.exception[k] = nil
-			end
-		end
-	end
-	if found then
-		self:Print("Removed " .. link .. " from exception exception list.")
-	end
-end
-
-function SellJunk:isException(link)
+function addon:isException(link)
 	local exception
 	_, _, link = string.find(link,"item:(%d+)")
-	if self.db.account.exception then
-		for k,v in pairs(self.db.account.exception) do
+	if self.db.global.exceptions then
+		for k,v in pairs(self.db.global.exceptions) do
 			_, _, exception = string.find(v,"item:(%d+)")
 			if exception == link then
 				return true
 			end
 		end
 	end
-	if self.db.char.exception then
-		for k,v in pairs(self.db.char.exception) do
+	if self.db.char.exceptions then
+		for k,v in pairs(self.db.char.exceptions) do
 			_, _, exception = string.find(v,"item:(%d+)")
 			if exception == link then
 				return true
@@ -211,4 +155,103 @@ function SellJunk:isException(link)
 		end
 	end
 	return false
+end
+
+function addon:OpenOptions()
+	InterfaceOptionsFrame_OpenToCategory(addon.optionsFrame)
+end
+
+function addon:PopulateOptions()
+	if not options then
+		options = {
+			type = "group",
+			name = "SellJunk",
+			args = {
+				general = {
+					order	= 1,
+					type	= "group",
+					name	= "global",
+					desc	= "Global settings",
+					args	= {
+						header1 = {
+							order	= 10,
+							type	= "description",
+							name	= "",
+						},
+						auto = {
+							order	= 11,
+							type = "toggle",
+							name = "Automatically sell junk at vendor?",
+							desc = "Toggles the automatic selling of junk when the merchant window is opened.",
+							get = function() return addon.db.char.auto end,
+							set = function() self.db.char.auto = not self.db.char.auto end,
+						},
+						header2 = {
+							order	= 12,
+							type	= "description",
+							name	= "",
+						},
+						list = {
+							order	= 13,
+							type = "execute",
+							name = "list all exceptions",
+							desc = "Lists all exceptions",
+							func = function() addon:List() end,
+						},
+						header3 = {
+							order	= 14,
+							type	= "description",
+							name	= "",
+						},
+						header4 = {
+							order	= 15,
+							type	= "header",
+							name	= "Global Exceptions",
+						},
+						add = {
+							order	= 16,
+							type = "input",
+							name = "Add item:",
+							desc = "Add an exception for all characters.",
+							usage = "<Item Link>",
+							get = false,
+							set = function(info, v) addon:Add(v, true) end,
+						},
+						rem = {
+							order	= 17,
+							type = "input",
+							name = "Remove item:",
+							desc = "Remove an exception for all characters.",
+							usage = "<Item Link>",
+							get = false,
+							set = function(info, v) addon:Rem(v, true) end,
+						},
+						header5 = {
+							order	= 18,
+							type	= "header",
+							name	= "Character Specific Exceptions",
+						},
+						addMe = {
+							order	= 19,
+							type = "input",
+							name = "Add item:",
+							desc = "Add an exception for this characters.",
+							usage = "<Item Link>",
+							get = false,
+							set = function(info, v) addon:Add(v, false) end,
+						},
+						remMe = {
+							order	= 20,
+							type = "input",
+							name = "Remove item:",
+							desc = "Remove an exception for this characters.",
+							usage = "<Item Link>",
+							get = false,
+							set = function(info, v) addon:Rem(v, false) end,
+						},
+					}
+				}
+			}
+		}
+	end
 end
