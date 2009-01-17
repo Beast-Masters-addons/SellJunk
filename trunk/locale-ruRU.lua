380950a836ed6c77a0b50fc1699601ea97ce197a
crazybenny <crazybenny@29a1b359-670f-4fa0-9745-e3f699048d19>
2009-01-17 05:38:59 +0000

    - updates for adding/removing exceptions logic - now using localization app

--- trunk/locale-ruRU.lua
+++ trunk/locale-ruRU.lua
@@ -2,30 +2,6 @@ local L = LibStub("AceLocale-3.0"):NewLocale("SellJunk", "ruRU")
 
 if L then
 
-L["Sold: "] = "Продано: "
-L["Global exception list:"] = "Общий список исключений:"
-L["Character exception list:"] = "Список исключений для персонажа:"
-L["Added "] = "Добавил "
-L[" to global exception list."] = " к общему списку исключений."
-L[" to character's exception list."] = " к списку исключений персонажа."
-L["Removed "] = "Удалил "
-L[" from global exception list."] = " из общего списка исключений."
-L[" from character's exception list."] = " из списка исключений персонажа."
-
--- options frame
-L["Automatically sell junk at vendor?"] = "Автопродажа?"
-L["Toggles the automatic selling of junk when the merchant window is opened."] = "Включить или отключить автоматическую продажу мусора при открытии окна торговли."
-L["List all exceptions"] = "Показать все исключения"
-L["Lists all exceptions"] = "Показывает все исключения"
-L["Global Exceptions"] = "Общие исключения"
-L["Drag item into this window to add/remove it from exception list"] = "Перетащите предмет в это окошко, чтобы добавить или удалить его из списка исключений."
-L["Add item:"] = "Добавить предмет:"
-L["Add an exception for all characters."] =  "Добавить исключение в общий список."
-L["Remove item:"] = "Удалить предмет:"
-L["Remove an exception for all characters."] = "Удалить исключение из общего списка."
-L["<Item Link>"] = "<Ссылка предмета>"
-L["Character Specific Exceptions"] = "Исключения для этого персонажа"
-L["Add an exception for this characters."] = "Добавить исключение в список этого персонажа."
-L["Remove an exception for this characters."] = "Удалить исключение из списка этого персонажа."
+--@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="english")@
 
 end
